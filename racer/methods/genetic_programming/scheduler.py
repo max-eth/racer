dfcabb3a3c0dfcbdb666d0dd6ee97f544606d438
Anton Schaefer <scanton@mit.edu>
2020-07-15 21:15:33 +0200

    Bugfix

--- racer/methods/genetic_programming/scheduler.py
+++ racer/methods/genetic_programming/scheduler.py
@@ -1,17 +1,17 @@
 class Scheduler:
     def __init__(self, *, milestones, **schedule):
-        self.step = 0
+        self.step_count = 0
         self.schedule = schedule
         self.milestones = milestones
 
     def step(self):
-        self.step += 1
+        self.step_count += 1
 
     def get(self, param_name):
         param_schedule = self.schedule[param_name]
         assert len(param_schedule) == len(self.milestones)
         i = 0
-        while self.step >= self.milestones[i] and i < len(param_schedule):
+        while self.step_count >= self.milestones[i] and i < len(param_schedule):
             val = param_schedule[i]
             i += 1
         return val
